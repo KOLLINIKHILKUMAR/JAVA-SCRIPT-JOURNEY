1b9aa08b89b0f2027539ba0a25151a3660b71b95
KOLLI NIKHIL KUMAR <nikhilkumarkolli.gmail.com>
2023-07-27 19:40:20 +0530

    dom

---
+++ 15_dom_project_1/script.js
@@ -0,0 +1,18 @@
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+
+let count=1;
+
+setInterval(()=>
+{
+    if(count<1000)
+    {
+        count++;
+        counter.innerText =count;
+    }
+},0.5);
+
+setTimeout(()=>
+{
+    followers.innerText="Followers on Instagram";
+},8000);
