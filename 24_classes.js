f76b61b0eb02f2e8841489c6b39fd22053428131
Nikhil kumar <103022251+KOLLINIKHILKUMAR@users.noreply.github.com>
2023-08-04 21:08:33 +0530

    inheritance

--- 24_classes.js
+++ 24_classes.js
@@ -13,9 +13,43 @@ class user{
     getCourseList(){   
         return this.#courseList;
     }
+
+    static login(){
+        return "You are logged in";
+    }
 }
 
 
-
-
 module.exports = user; // exporting the class
+
+
+const nikki = new user('Nikki', 'nikki@nikki.com');
+
+// console.log(nikki.getInfo());
+nikki.enrollCourse('React Bootcamp');
+nikki.enrollCourse('Angular Bootcamp');
+// console.log(nikki.getCourseList());
+// console.log(nikki.courseList); // undefined
+
+
+class subadmin extends user{
+    constructor(name, email){
+        super(name, email);
+    }
+    getAdminInfo(){
+        return "I am subadmin";
+    }
+    login(){
+        return "Login for sub admin only";
+    }
+}
+
+
+const tom = new subadmin('tom', 'tom@tom.com');
+
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+console.log(tom.getInfo());
+
+
+
--- 24_classesusage.js
+++ 24_classesusage.js
@@ -1,4 +1,16 @@
 // import user from './24_classes.js'; // importing the class
 
 // Path: 24_classesusage.js
-const EventEmitter = require('./24_classes.js');
+const user = require('./24_classes.js');
+
+const nikhil = new user('Nikhil', 'nikhilkumarkolli@gmail.com');
+
+console.log(nikhil.getInfo());
+nikhil.enrollCourse('React Bootcamp');
+nikhil.enrollCourse('Angular Bootcamp');
+
+console.log(nikhil.getCourseList());
+
+let courses = nikhil.getCourseList();
+
+courses.forEach((c) => console.log(c));
